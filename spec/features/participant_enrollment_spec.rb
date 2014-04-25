85dbf56c37c6f51893e42fd5c44ea2e73c84776d
Tag0Mag0 <jem.hilton@gmail.com>
2014-04-24 21:20:21 -0500

    added first spec for pending participants

---
+++ spec/features/participant_enrollment_spec.rb
@@ -0,0 +1,16 @@
+require "spec_helper"
+
+describe "participant enrollment" do
+  fixtures(
+    :users, :participants
+  )
+
+  before do
+    sign_in_user users(:admin1)
+    visit "/en/pending/participants"
+  end
+
+  it "should show a list of pending participants" do
+    expect(page).to have_text(participant(:participant1).study_identifier)
+  end
+end
