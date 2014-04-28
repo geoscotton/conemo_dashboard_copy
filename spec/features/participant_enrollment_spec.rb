12daedefc749d6742fc55a842d5fd121ee1d46f0
Eric Carty-Fickes <ericcf@northwestern.edu>
2014-04-28 11:59:42 -0500

    fix rubocop errors [skip ci]

--- spec/features/participant_enrollment_spec.rb
+++ spec/features/participant_enrollment_spec.rb
@@ -24,13 +24,13 @@ describe "participant enrollment" do
     fill_in "Family record number", with: "12345"
     fill_in "participant_phone", with: "224-444-5555"
     fill_in("Email", with: "joe")
-    select "2011", from: 'participant_date_of_birth_1i'
-    select "March", from: 'participant_date_of_birth_2i'
-    select "1", from: 'participant_date_of_birth_3i'
+    select "2011", from: "participant_date_of_birth_1i"
+    select "March", from: "participant_date_of_birth_2i"
+    select "1", from: "participant_date_of_birth_3i"
     fill_in "Address", with: "Address of participant"
-    select "2014", from: 'participant_enrollment_date_1i'
-    select "August", from: 'participant_enrollment_date_2i'
-    select "1", from: 'participant_enrollment_date_3i'
+    select "2014", from: "participant_enrollment_date_1i"
+    select "August", from: "participant_enrollment_date_2i"
+    select "1", from: "participant_enrollment_date_3i"
     choose "participant_gender_male"
     choose "participant_key_chronic_disorder_diabetes"
     click_on("Save")
