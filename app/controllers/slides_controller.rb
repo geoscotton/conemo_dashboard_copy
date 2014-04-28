c6bc8e125007a18684b99eb330724bd223cfb7e5
Eric Carty-Fickes <ericcf@northwestern.edu>
2014-04-28 15:43:46 -0500

    more lesson management and export functionality

--- app/controllers/slides_controller.rb
+++ app/controllers/slides_controller.rb
@@ -2,6 +2,11 @@
 class SlidesController < ApplicationController
   before_action :find_lesson
 
+  def show
+    @slide = @lesson.find_slide(params[:id])
+    authorize! :show, @slide
+  end
+
   def new
     @slide = @lesson.build_slide
     authorize! :new, @slide
@@ -15,7 +20,8 @@ class SlidesController < ApplicationController
       redirect_to lesson_url(@lesson),
                   notice: I18n.t("conemo.controllers.slides.saved")
     else
-      flash.now[:alert] = I18n.t("conemo.controllers.slides.not_saved")
+      flash.now[:alert] = I18n.t("conemo.controllers.slides.not_saved") +
+        ": " + @slide.errors.full_messages.join(", ")
       render :new
     end
   end
@@ -27,6 +33,6 @@ class SlidesController < ApplicationController
   end
 
   def slide_params
-    params.require(:slide).permit(:title, :body)
+    params.require(:slide).permit(:title, :body, :position)
   end
 end
--- app/views/lessons/_lesson.json.jbuilder
+++ app/views/lessons/_lesson.json.jbuilder
@@ -3,3 +3,5 @@ json.title lesson.title
 json.dayInTreatment lesson.day_in_treatment
 json.l10n lesson.locale
 json.guid lesson.guid
+
+json.slides lesson.slides, partial: "slides/slide", as: :slide
