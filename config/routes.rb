4c7dc37e4a9ab93661aae79bcf72a83d3ea85e06
Tag0Mag0 <jem.hilton@gmail.com>
2014-04-28 11:35:25 -0500

    removed default locale from routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  scope "/(:locale)", defaults: { locale: "en" } do
+  scope "/(:locale)" do
     devise_for :users
 
     mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
