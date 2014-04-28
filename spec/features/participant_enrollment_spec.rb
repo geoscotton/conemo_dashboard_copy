60c2e0df79e0caef300a428d4ca856270d3a93f5
Tag0Mag0 <jem.hilton@gmail.com>
2014-04-28 09:22:07 -0500

    updated fixtures

--- spec/features/participant_enrollment_spec.rb
+++ spec/features/participant_enrollment_spec.rb
@@ -5,12 +5,31 @@ describe "participant enrollment" do
     :users, :participants
   )
 
-  before do
+  before(:each) do
     sign_in_user users(:admin1)
-    visit "/en/pending/participants"
   end
 
   it "should show a list of pending participants" do
+    visit "/en/pending/participants"
     expect(page).to have_text(participants(:participant1).study_identifier)
+  end
+
+  it "should enroll a new participant" do
+    visit "/en/participants/new"
+    expect(page).to have_text "Enroll New Participant"
+    fill_in("First name", with: "Joe")
+    fill_in("Last name", with: "Blow")
+    fill_in("Study Identifier", with: "s14")
+    fill_in("Family health unit name", with: "Healthy!")
+    fill_in("Family record number", with: "12345")
+    fill_in("Phone", with: "224-444-5555")
+    fill_in("Email", with: "joe")
+    select '2011/01/01', from: 'Date of birth'
+    fill_in("Address", with: "Joe")
+    select '2011/01/01', from: 'Enrollment date'
+    fill_in("gender", with: "Joe")
+    fill_in("Key chronic disorder", with: "Joe")
+    click_on("Save")
+    expect(page).to have_text "Successfully created participant"
   end
 end
