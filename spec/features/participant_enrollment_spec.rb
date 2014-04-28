4a9650eda376a6d59271ed0f6d5f5b0c15a87fbe
Tag0Mag0 <jem.hilton@gmail.com>
2014-04-28 09:49:10 -0500

    updated enrollment feature spec

--- spec/features/participant_enrollment_spec.rb
+++ spec/features/participant_enrollment_spec.rb
@@ -17,18 +17,22 @@ describe "participant enrollment" do
   it "should enroll a new participant" do
     visit "/en/participants/new"
     expect(page).to have_text "Enroll New Participant"
-    fill_in("First name", with: "Joe")
-    fill_in("Last name", with: "Blow")
+    fill_in "First name", with: "Joe"
+    fill_in "Last name", with: "Blow"
     fill_in("Study Identifier", with: "s14")
-    fill_in("Family health unit name", with: "Healthy!")
-    fill_in("Family record number", with: "12345")
-    fill_in("Phone", with: "224-444-5555")
+    fill_in "Family health unit name", with: "Healthy!"
+    fill_in "Family record number", with: "12345"
+    fill_in "participant_phone", with: "224-444-5555"
     fill_in("Email", with: "joe")
-    select '2011/01/01', from: 'Date of birth'
-    fill_in("Address", with: "Joe")
-    select '2011/01/01', from: 'Enrollment date'
-    fill_in("gender", with: "Joe")
-    fill_in("Key chronic disorder", with: "Joe")
+    select "2011", from: 'participant_date_of_birth_1i'
+    select "March", from: 'participant_date_of_birth_2i'
+    select "1", from: 'participant_date_of_birth_3i'
+    fill_in "Address", with: "Address of participant"
+    select "2014", from: 'participant_enrollment_date_1i'
+    select "August", from: 'participant_enrollment_date_2i'
+    select "1", from: 'participant_enrollment_date_3i'
+    choose "participant_gender_male"
+    choose "participant_key_chronic_disorder_diabetes"
     click_on("Save")
     expect(page).to have_text "Successfully created participant"
   end
