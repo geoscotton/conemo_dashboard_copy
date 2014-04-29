5fdb5f0f898b1993f1d5ce17f4bab0b6b5fb40c8
Eric Carty-Fickes <ericcf@northwestern.edu>
2014-04-29 08:14:31 -0500

    cleaned up and added specs to participants controllers

--- app/controllers/active/participants_controller.rb
+++ app/controllers/active/participants_controller.rb
@@ -4,9 +4,5 @@ module Active
     def index
       @participants = Participant.active
     end
-
-    def show
-      @participant = Participant.where(id: params[:id]).first
-    end
   end
 end
--- app/controllers/pending/participants_controller.rb
+++ app/controllers/pending/participants_controller.rb
@@ -1,17 +1,21 @@
 module Pending
   # Managing pending participants.
   class ParticipantsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
     def index
       @participants = Participant.pending
     end
 
-    def show
-      @participant = Participant.where(id: params[:id]).first
+    def activate
+      @participant = Participant.pending.find(params[:id])
+      @nurses = User.where(role: "nurse")
     end
 
-    def activate
-      @participant = Participant.where(id: params[:id]).first
-      @nurses = User.where(role: "nurse")
+    private
+
+    def record_not_found
+      redirect_to pending_participants_url, alert: "Participant not found"
     end
   end
 end
