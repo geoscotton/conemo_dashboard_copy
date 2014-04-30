fa914364713bfb418d4d7678be2118615e1bb8fe
Tag0Mag0 <jem.hilton@gmail.com>
2014-04-30 15:20:57 -0500

    finished second contact creation feature
    [Finishes #70097284]

--- app/models/second_contact.rb
+++ app/models/second_contact.rb
@@ -1,5 +1,6 @@
 # Information gathered by phone after First Appointment
 class SecondContact < ActiveRecord::Base
+  model_name.instance_variable_set :@route_key, "second_contact"
   belongs_to :participant
   has_one :nurse_participant_evaluation, dependent: :destroy
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140430180404) do
+ActiveRecord::Schema.define(version: 20140430201617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -152,7 +152,7 @@ ActiveRecord::Schema.define(version: 20140430180404) do
 
   create_table "second_contacts", force: true do |t|
     t.integer  "participant_id",                null: false
-    t.date     "contact_at",                    null: false
+    t.datetime "contact_at",                    null: false
     t.boolean  "video_access",   default: true
     t.integer  "session_length",                null: false
     t.text     "notes"
