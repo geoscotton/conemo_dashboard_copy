18dedc3c830472157c07d70bcae7ba8478028ea5
Tag0Mag0 <jem.hilton@gmail.com>
2014-05-06 16:25:19 -0500

    rubocop code cleanup

--- app/models/content_access_event.rb
+++ app/models/content_access_event.rb
@@ -1,3 +1,4 @@
+# Patient app use data imported from PRI
 class ContentAccessEvent < ActiveRecord::Base
   belongs_to :lesson
   belongs_to :participant
--- app/models/response.rb
+++ app/models/response.rb
@@ -1,3 +1,4 @@
+# Patient response in phone app imported from PRI
 class Response < ActiveRecord::Base
   belongs_to :content_access_event
 end
