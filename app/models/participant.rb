c870e08d45437e14eb47f6a72340d6e811bc0356
Tag0Mag0 <jem.hilton@gmail.com>
2014-05-07 12:44:00 -0500

    Implemented overall status feature
    [Finishes #70097508]

--- app/models/participant.rb
+++ app/models/participant.rb
@@ -50,13 +50,19 @@ class Participant < ActiveRecord::Base
   end
 
   def study_day
-    (Date.today - start_date).to_i
+    if start_date
+      (Date.today - start_date).to_i
+    end
   end
 
-  def current_lesson
-  end
-
-  def previous_lesson
+  def current_study_status
+    if current_lesson && previous_lesson
+      two_lessons_passed
+    elsif current_lesson
+      one_lesson_passed
+    else
+      nil
+    end
   end
 
   private
@@ -67,6 +73,48 @@ class Participant < ActiveRecord::Base
         :enrollment_date,
         I18n.t("conemo.models.participant.enrollment_date_is_sane_error")
       )
+    end
+  end
+
+  def current_lesson
+    Lesson.where("day_in_treatment <= ?", study_day)
+          .order(day_in_treatment: :desc).second
+  end
+
+  def previous_lesson
+    Lesson.where("day_in_treatment <= ?", study_day)
+          .order(day_in_treatment: :desc).offset(2).first
+  end
+
+  def current_lesson_complete?
+    if current_lesson
+      current_lesson.content_access_events.where(participant_id: id).any?
+    end
+  end
+
+  def previous_lesson_complete?
+    if previous_lesson
+      previous_lesson.content_access_events.where(participant_id: id).any?
+    end
+  end
+
+  def two_lessons_passed
+    if  current_lesson_complete? == false &&
+        previous_lesson_complete? == false
+      "danger"
+    elsif current_lesson_complete? == false ||
+          previous_lesson_complete? == false
+      "warning"
+    else
+      "stable"
+    end
+  end
+
+  def one_lesson_passed
+    if current_lesson_complete?
+      "stable"
+    else
+      "warning"
     end
   end
 end
