b5d1c15a564659f303e1e11a7960d32ebaca902d
Tag0Mag0 <jem.hilton@gmail.com>
2014-05-07 15:42:23 -0500

    Finished lesson access notifications
    [Finishes #70543062] [Finishes #70543130]

---
+++ app/controllers/concerns/status.rb
@@ -0,0 +1,88 @@
+# Handles Participant lesson status logic
+module Status
+  extend ActiveSupport::Concern
+
+  def lesson_status(lesson)
+    if lesson_released?(lesson)
+      access_status(lesson)
+    else
+      "un-released"
+    end
+  end
+
+  def current_study_status
+    if current_lesson && previous_lesson
+      two_lessons_passed
+    elsif current_lesson
+      one_lesson_passed
+    else
+      nil
+    end
+  end
+
+  def lesson_released?(lesson)
+    lesson.day_in_treatment <= study_day
+  end
+
+  def next_lesson(lesson)
+    Lesson.where("day_in_treatment > ?", lesson.day_in_treatment)
+          .order(day_in_treatment: :asc).first
+  end
+
+  def access_status(lesson)
+    access = content_access_events.where(lesson_id: lesson.id).first
+    if !access && lesson_released?(next_lesson(lesson))
+      "danger"
+    elsif !access
+      "info"
+    elsif access.late?
+      "warning"
+    else
+      "success"
+    end
+  end
+
+  private
+
+  def current_lesson
+    Lesson.where("day_in_treatment <= ?", study_day)
+          .order(day_in_treatment: :desc).second
+  end
+
+  def previous_lesson
+    Lesson.where("day_in_treatment <= ?", study_day)
+          .order(day_in_treatment: :desc).offset(2).first
+  end
+
+  def current_lesson_complete?
+    if current_lesson
+      current_lesson.content_access_events.where(participant_id: id).any?
+    end
+  end
+
+  def previous_lesson_complete?
+    if previous_lesson
+      previous_lesson.content_access_events.where(participant_id: id).any?
+    end
+  end
+
+  def two_lessons_passed
+    if  !current_lesson_complete? &&
+        !previous_lesson_complete?
+      "danger"
+    elsif !current_lesson_complete? ||
+          !previous_lesson_complete?
+      "warning"
+    else
+      "stable"
+    end
+  end
+
+  def one_lesson_passed
+    if current_lesson_complete?
+      "stable"
+    else
+      "warning"
+    end
+  end
+end
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,5 +1,7 @@
 # Study Participant
 class Participant < ActiveRecord::Base
+  include Status
+
   belongs_to :nurse, class_name: "User", foreign_key: :nurse_id
   has_one :first_contact, dependent: :destroy
   has_one :first_appointment, dependent: :destroy
@@ -55,16 +57,6 @@ class Participant < ActiveRecord::Base
     end
   end
 
-  def current_study_status
-    if current_lesson && previous_lesson
-      two_lessons_passed
-    elsif current_lesson
-      one_lesson_passed
-    else
-      nil
-    end
-  end
-
   private
 
   def enrollment_date_is_sane
@@ -73,48 +65,6 @@ class Participant < ActiveRecord::Base
         :enrollment_date,
         I18n.t("conemo.models.participant.enrollment_date_is_sane_error")
       )
-    end
-  end
-
-  def current_lesson
-    Lesson.where("day_in_treatment <= ?", study_day)
-          .order(day_in_treatment: :desc).second
-  end
-
-  def previous_lesson
-    Lesson.where("day_in_treatment <= ?", study_day)
-          .order(day_in_treatment: :desc).offset(2).first
-  end
-
-  def current_lesson_complete?
-    if current_lesson
-      current_lesson.content_access_events.where(participant_id: id).any?
-    end
-  end
-
-  def previous_lesson_complete?
-    if previous_lesson
-      previous_lesson.content_access_events.where(participant_id: id).any?
-    end
-  end
-
-  def two_lessons_passed
-    if  current_lesson_complete? == false &&
-        previous_lesson_complete? == false
-      "danger"
-    elsif current_lesson_complete? == false ||
-          previous_lesson_complete? == false
-      "warning"
-    else
-      "stable"
-    end
-  end
-
-  def one_lesson_passed
-    if current_lesson_complete?
-      "stable"
-    else
-      "warning"
     end
   end
 end
