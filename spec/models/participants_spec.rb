5889b8af5c2ee68e08aab2932445a42b246c69cc
Tag0Mag0 <jem.hilton@gmail.com>
2014-05-08 17:38:24 -0500

    expanded test coverage for participant status

--- spec/models/participants_spec.rb
+++ spec/models/participants_spec.rb
@@ -96,14 +96,78 @@ describe Participant do
   end
 
   describe "Lesson Status" do
+    let(:participant_day_5) { participants(:active_participant_day_5) }
+
     describe "#next_lesson" do
       let(:next_lesson) { lessons(:day5) }
-      let(:participant_day_5) { participants(:active_participant_day_5) }
       let(:current_lesson) { lessons(:day4) }
 
       it "should return the next lesson" do
         result = participant_day_5.next_lesson(current_lesson)
         expect(result).to eq next_lesson
+      end
+    end
+
+    describe "#lesson_released(lesson)" do
+      context "lesson day in treatment is greater than current day" do
+        let(:unreleased_lesson) { lessons(:day6) }
+
+        it "should return false" do
+          result = participant_day_5.lesson_released?(unreleased_lesson)
+          expect(result).to eq false
+        end
+      end
+
+      context "lesson day in treatment is not greater than current day" do
+        let(:released_lesson) { lessons(:day4) }
+
+        it "should return true" do
+          result = participant_day_5.lesson_released?(released_lesson)
+          expect(result).to eq true
+        end
+      end
+    end
+
+    describe "#access_status(lesson)" do
+      let(:missed_lesson) { lessons(:day2) }
+      let(:current_lesson) { lessons(:day5) }
+      let(:late_lesson) { lessons(:day4) }
+      let(:on_time_lesson) { lessons(:day4) }
+
+      context "lesson has not been accessed and next lesson has been released" do
+        it "should return 'danger'" do
+          result = participant_day_5.access_status(missed_lesson)
+          expect(result).to eq "danger"
+        end
+      end
+
+      context "lesson has not been accessed but next lesson has not been released" do
+        it "should return 'info'" do
+          result = participant_day_5.access_status(current_lesson)
+          expect(result).to eq "info"
+        end
+      end
+
+      context "lesson has been accessed but it was late" do
+        it "should return 'warning'" do
+          ContentAccessEvent.create(participant_id: participant_day_5.id,
+                                    day_in_treatment_accessed: 5,
+                                    lesson_id: late_lesson.id
+                                   )
+          result = participant_day_5.access_status(late_lesson)
+          expect(result).to eq "warning"
+        end
+      end
+
+      context "lesson was accessed on time" do
+        it "should return 'success'" do
+          ContentAccessEvent.create(participant_id: participant_day_5.id,
+                                    day_in_treatment_accessed: on_time_lesson.day_in_treatment,
+                                    lesson_id: on_time_lesson.id
+                                   )
+          result = participant_day_5.access_status(on_time_lesson)
+          expect(result).to eq "success"
+        end
       end
     end
   end
