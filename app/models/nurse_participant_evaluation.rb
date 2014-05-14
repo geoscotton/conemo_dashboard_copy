239298eff617c78fe0f9e88888fd49e5f80b092a
Tag0Mag0 <jem.hilton@gmail.com>
2014-05-14 15:26:38 -0500

    fixed model dependencies

--- app/models/nurse_participant_evaluation.rb
+++ app/models/nurse_participant_evaluation.rb
@@ -1,7 +1,7 @@
 # Nurse evaluation of Participant for First Appointment and Second Contact
 class NurseParticipantEvaluation < ActiveRecord::Base
-  belongs_to :first_appointment, dependent: :destroy
-  belongs_to :second_contact, dependent: :destroy
+  belongs_to :first_appointment
+  belongs_to :second_contact
 
   validates :smartphone_comfort,
             :participant_session_engagement,
