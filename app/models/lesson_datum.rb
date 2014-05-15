e632a2c3be606f31b9196b18ceff48eb9dc7ad33
Tag0Mag0 <jem.hilton@gmail.com>
2014-05-15 12:30:55 -0500

    added rake task to schedule.rb
    scoped lesson list to locale

--- app/models/lesson_datum.rb
+++ app/models/lesson_datum.rb
@@ -2,7 +2,7 @@ class LessonDatum < ActiveRecord::Base
   establish_connection :prw
 
   def content_access_event_exists?
-    ContentAccessEvent.exists?(lesson_datum_guid: guid)
+    ContentAccessEvent.exists?(lesson_datum_guid: self.GUID)
   end
 
   def parse_responses
