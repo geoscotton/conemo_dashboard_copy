d689b4b8b83cbb392648e0dda0628e001e833806
Tag0Mag0 <jem.hilton@gmail.com>
2014-05-20 08:53:26 -0300

    added locale to users for sms

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140516191848) do
+ActiveRecord::Schema.define(version: 20140520114433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -264,6 +264,7 @@ ActiveRecord::Schema.define(version: 20140516191848) do
     t.string   "phone",                               null: false
     t.string   "first_name",                          null: false
     t.string   "last_name",                           null: false
+    t.string   "locale"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
