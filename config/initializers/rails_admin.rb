c82589baaf0b4b0c7da9d8eb57cf1ae946b631b7
Tag0Mag0 <jem.hilton@gmail.com>
2014-06-04 13:24:49 -0500

    exposed reminder message model in rails admin interface

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -27,7 +27,7 @@ RailsAdmin.config do |config|
     delete
     show_in_app
 
-    config.included_models = [User, Participant]
+    config.included_models = [User, Participant, ReminderMessage]
 
     config.model User do
     
