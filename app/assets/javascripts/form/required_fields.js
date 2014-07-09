b3c8e0bc090fb049723759c990ae6814ef30d28e
Tag0Mag0 <jem.hilton@gmail.com>
2014-07-09 17:33:09 -0500

    added dropdown library for selects

--- app/assets/javascripts/form/required_fields.js
+++ app/assets/javascripts/form/required_fields.js
@@ -5,6 +5,19 @@
             .closest(".form-group")
             .children("label")
             .append(" <i class='fa fa-asterisk text-danger'></i>");
+        $('.dropdown').select2({
+          width: "100%",
+          minimumResultsForSearch: -1,
+          allowClear: true,
+          containerCss: {
+            "margin-top": "12px",
+          },
+          dropdownCss: {
+            "left": "10%",
+            "width": "75%"
+          }
+        });
+        
     }
 
     $(document).on("page:change", styleFields);
