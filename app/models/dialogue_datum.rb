8ae88f55215f1c62a18c48cf596b8fdef8ae586b
Tag0Mag0 <jem.hilton@gmail.com>
2014-07-31 10:07:03 -0500

    updated prw import

--- app/models/dialogue_datum.rb
+++ app/models/dialogue_datum.rb
@@ -3,6 +3,6 @@ class DialogueDatum < ActiveRecord::Base
   establish_connection :prw
 
   def content_access_event_exists?
-    ContentAccessEvent.exists?(dialogue_datum_guid: self.FEATURE_VALUE_DT_dialogue_guid)
+    ContentAccessEvent.exists?(dialogue_datum_guid: self.GUID)
   end
 end
