1c5f33eb85344256cd8fcbbf2237a7d6df1d1909
Eric Carty-Fickes <ericcf@northwestern.edu>
2015-05-13 10:20:22 -0500

    ignore unrecognized request exceptions
    [#94494002] [#94494098] [#92926888]

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -2,6 +2,10 @@ require "raven"
 
 Raven.configure do |config|
   config.environments = %w( staging production )
+  config.excluded_exceptions = [
+    "URI::InvalidURIError",
+    "ActionController::UnknownHttpMethod"
+  ]
   dsn = Rails.application.config.try(:sentry_dsn)
   config.dsn = dsn if dsn
 end
