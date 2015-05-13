686579422f5c408a5bda5e7b49284d2115871b35
Eric Carty-Fickes <ericcf@northwestern.edu>
2015-05-13 17:09:15 -0500

    fix broken controller and add spec
    [#94529644]

--- spec/controllers/third_contacts_controller_spec.rb
+++ spec/controllers/third_contacts_controller_spec.rb
@@ -1,6 +1,8 @@
 require "spec_helper"
 
 RSpec.describe ThirdContactsController, type: :controller do
+  fixtures :all
+
   describe ".filter_params" do
     it "permits expected parameters" do
       raw_params = {
@@ -39,6 +41,41 @@ RSpec.describe ThirdContactsController, type: :controller do
 
       expect(filtered_params)
         .to eq(raw_params[:third_contact].with_indifferent_access)
+    end
+  end
+
+  describe "POST create" do
+    context "when unauthenticated" do
+      it "responds with 302" do
+        post :create, participant_id: Participant.first.id
+
+        expect(response.status).to eq 302
+      end
+    end
+
+    context "when authenticated" do
+      context "and the contact saves successfully" do
+        it "redirects to the active participants" do
+          user = User.first
+          participant = instance_double("Participant")
+          contact = instance_double("ThirdContact",
+                                    save: true,
+                                    schedule_message: nil)
+          sign_in_user user
+          allow(Participant).to receive(:find) { participant }
+
+          expect(participant).to receive(:build_third_contact)
+            .with(instance_of(ActionController::Parameters))
+            .and_return(contact)
+
+          post :create,
+               participant_id: Participant.first.id,
+               third_contact: { q1: 'question 1' },
+               locale: user.locale
+
+          expect(response).to redirect_to(active_participants_path)
+        end
+      end
     end
   end
 end
