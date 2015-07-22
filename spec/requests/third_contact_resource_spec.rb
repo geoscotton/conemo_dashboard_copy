885abb2aecc8e453a2c9b48a1c8760bf607f1e98
Eric Carty-Fickes <ericcf@northwestern.edu>
2015-07-22 14:16:02 -0500

    redirect requests to third_contact#show
    [#94709102]

---
+++ spec/requests/third_contact_resource_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe "Third Contact resource requests", type: :request do
+  describe "GET show" do
+    it "redirects to the active participants index" do
+      get "/en/participants/123/third_contact"
+
+      expect(response).to redirect_to("/en/active/participants")
+    end
+  end
+end
