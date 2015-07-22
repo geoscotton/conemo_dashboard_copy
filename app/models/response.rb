6c2d6365ce06061761e66633b834d1c4ccab61fb
Eric Carty-Fickes <ericcf@northwestern.edu>
2015-07-22 08:09:06 -0500

    ensure all model exports include a locale
    * the locale is associated with a Participant, so this requires
      all exportable models to include an association with a
      Participant (models that have a logical association)
    
    [#96497206]

--- app/models/response.rb
+++ app/models/response.rb
@@ -1,6 +1,9 @@
 # Patient response in phone app imported from PRI
 class Response < ActiveRecord::Base
   belongs_to :content_access_event
+
+  has_one :participant,
+          through: :content_access_event
   
   def parse_responses
     JSON.parse(answer)
