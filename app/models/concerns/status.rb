53f4233cee941711bcaadec07dba8043a1ed7f15
Eric Carty-Fickes <ericcf@northwestern.edu>
2015-08-27 21:29:00 -0500

    rearrange and note possible private methods

--- app/models/concerns/status.rb
+++ app/models/concerns/status.rb
@@ -25,87 +25,8 @@ module Status
     end
   end
 
-  def lesson_released?(lesson)
-    if lesson && study_day
-      lesson.day_in_treatment <= study_day
-    else
-      false
-    end
-  end
-
-  def access_status(lesson)
-    access = access_response(lesson)
-
-    if lesson.guid == current_lesson.guid
-      "info"
-    elsif !access && lesson_released?(next_lesson(lesson))
-      "danger"
-    elsif access && access.late?
-      "warning"
-    else
-      "success"
-    end
-  end
-
   def access_response(lesson)
     content_access_events.where(lesson_id: lesson.id).first
-  end
-
-  def next_lesson(lesson)
-    Lesson.where("day_in_treatment > ?", lesson.day_in_treatment)
-    .where(locale: locale)
-    .order(day_in_treatment: :asc).first
-  end
-
-  # Overall Status
-  def current_lesson
-    Lesson.where("day_in_treatment <= ?", study_day)
-    .where(locale: locale)
-    .order(day_in_treatment: :desc).first
-  end
-
-  def one_lesson_ago
-    Lesson.where("day_in_treatment <= ?", study_day)
-    .where(locale: locale)
-    .order(day_in_treatment: :desc).second
-  end
-
-  def two_lessons_ago
-    Lesson.where("day_in_treatment <= ?", study_day)
-    .where(locale: locale)
-    .order(day_in_treatment: :desc).offset(2).first
-  end
-
-  def two_lessons_ago_complete?
-    if two_lessons_ago
-      two_lessons_ago.content_access_events.where(participant_id: id).any?
-    end
-  end
-
-  def one_lesson_ago_complete?
-    if one_lesson_ago
-      one_lesson_ago.content_access_events.where(participant_id: id).any?
-    end
-  end
-
-  def two_lessons_passed
-    if  !one_lesson_ago_complete? &&
-        !two_lessons_ago_complete?
-      "danger"
-    elsif !one_lesson_ago_complete? ||
-        !two_lessons_ago_complete?
-      "warning"
-    else
-      "stable"
-    end
-  end
-
-  def one_lesson_passed
-    if one_lesson_ago_complete?
-      "stable"
-    else
-      "warning"
-    end
   end
 
   def current_study_status
@@ -121,6 +42,95 @@ module Status
       end
     else
       "none"
+    end
+  end
+
+  # TODO: make private?
+  def lesson_released?(lesson)
+    if lesson && study_day
+      lesson.day_in_treatment <= study_day
+    else
+      false
+    end
+  end
+
+  # TODO: make private?
+  def access_status(lesson)
+    access = access_response(lesson)
+
+    if lesson.guid == current_lesson.guid
+      "info"
+    elsif !access && lesson_released?(next_lesson(lesson))
+      "danger"
+    elsif access && access.late?
+      "warning"
+    else
+      "success"
+    end
+  end
+
+  # TODO: make private?
+  def next_lesson(lesson)
+    Lesson.where("day_in_treatment > ?", lesson.day_in_treatment)
+    .where(locale: locale)
+    .order(day_in_treatment: :asc).first
+  end
+
+  # TODO: make private?
+  # Overall Status
+  def current_lesson
+    Lesson.where("day_in_treatment <= ?", study_day)
+    .where(locale: locale)
+    .order(day_in_treatment: :desc).first
+  end
+
+  # TODO: make private?
+  def one_lesson_ago
+    Lesson.where("day_in_treatment <= ?", study_day)
+    .where(locale: locale)
+    .order(day_in_treatment: :desc).second
+  end
+
+  # TODO: make private?
+  def two_lessons_ago
+    Lesson.where("day_in_treatment <= ?", study_day)
+    .where(locale: locale)
+    .order(day_in_treatment: :desc).offset(2).first
+  end
+
+  # TODO: make private?
+  def two_lessons_ago_complete?
+    if two_lessons_ago
+      two_lessons_ago.content_access_events.where(participant_id: id).any?
+    end
+  end
+
+  # TODO: make private?
+  def one_lesson_ago_complete?
+    if one_lesson_ago
+      one_lesson_ago.content_access_events.where(participant_id: id).any?
+    end
+  end
+
+  # TODO: make private?
+  def two_lessons_passed
+    if  !one_lesson_ago_complete? &&
+        !two_lessons_ago_complete?
+      "danger"
+    elsif !one_lesson_ago_complete? ||
+        !two_lessons_ago_complete?
+      "warning"
+    else
+      "stable"
+    end
+  end
+
+  # TODO: make private?
+  def one_lesson_passed
+    if one_lesson_ago_complete?
+      "stable"
+    else
+      "warning"
     end
   end
 end
