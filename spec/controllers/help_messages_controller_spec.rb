3e6e864abfc14ceca75c691e461ac24231331c11
Eric Carty-Fickes <ericcf@northwestern.edu>
2015-11-12 12:23:42 -0600

    add HelpMessagesController specs

--- spec/controllers/help_messages_controller_spec.rb
+++ spec/controllers/help_messages_controller_spec.rb
@@ -2,4 +2,18 @@ require "spec_helper"
 
 RSpec.describe HelpMessagesController, type: :controller do
   fixtures :all
+
+  let(:participant) { Participant.first }
+
+  let(:help_message) { HelpMessage.first }
+
+  describe "PUT update" do
+    context "for unauthenticated requests" do
+      before do
+        put :update, participant_id: participant.id, id: help_message.id
+      end
+
+      it_behaves_like "a rejected user action"
+    end
+  end
 end
