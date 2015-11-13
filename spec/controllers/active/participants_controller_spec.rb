590cb065c1b56dcfd96f66c88552387255ea759c
Eric Carty-Fickes <ericcf@northwestern.edu>
2015-11-13 14:27:40 -0600

    add Active::ParticipantsController specs

--- spec/controllers/active/participants_controller_spec.rb
+++ spec/controllers/active/participants_controller_spec.rb
@@ -2,19 +2,52 @@ require "spec_helper"
 
 module Active
   describe ParticipantsController, type: :controller do
+    LOCALES = %w( en es-PE pt-BR )
+
     fixtures :all
 
-    describe "GET index" do
-      let(:user) { double("user", admin?: true, nurse?: false, timezone: "Central Time (US & Canada)") }
-      context "for authenticated requests" do
-        before do
-          sign_in_user user
-          get :index
-        end
+    let(:participant) { Participant.first }
 
+    describe "GET index" do
+      context "for authenticated requests" do
         it "should render the index page" do
+          admin_request :get, :index
+
           expect(response).to render_template :index
         end
+
+        context "when the User is an admin" do
+          it "sets participants not scoped by User" do
+            LOCALES.each do |locale|
+              admin_request :get, :index, locale: locale
+
+              expect(assigns(:participants).count)
+                .to eq Participant.where(locale: locale).active.count
+            end
+          end
+        end
+
+        context "when the User is not an admin" do
+          it "sets participants scoped by User" do
+            nurse = sign_in_nurse
+            participants = Participant.where(locale: nurse.locale)
+            allow(nurse).to receive(:active_participants)
+              .and_return(participants)
+
+            nurse_request :get, :index, locale: nurse.locale
+
+            expect(assigns(:participants).count)
+              .to eq participants.count
+          end
+        end
+      end
+    end
+
+    describe "GET show" do
+      it "sets the participant" do
+        admin_request :get, :show, id: participant.id
+
+        expect(assigns(:participant)).to eq participant
       end
     end
   end
