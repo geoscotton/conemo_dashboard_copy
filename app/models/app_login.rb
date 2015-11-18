93ab0f50ed25d2841690eb010260a7cd4b6e0ad3
Eric Carty-Fickes <ericcf@northwestern.edu>
2015-11-18 16:38:52 -0600

    deleted commented code

--- app/models/app_login.rb
+++ app/models/app_login.rb
@@ -5,12 +5,4 @@ class AppLogin < ActiveRecord::Base
   alias_attribute :participant_identifier, :FEATURE_VALUE_DT_user_id
   alias_attribute :login, :eventDateTime
   alias_attribute :guid, :GUID
-
-  # def zonedEventDateTime
-  #   date_and_time = "%m-%d-%Y %H:%M:%S %Z"
-  #   unzoned = eventDateTime.in_time_zone('UTC').strftime("%m-%d-%Y %H:%M:%S")
-  #   zone = ConemoDashboard::Application.config.time_zone
-
-  #   DateTime.strptime("#{ unzoned } #{ zone }", date_and_time)
-  # end
 end
