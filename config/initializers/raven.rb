1b5597530a68e0d84f61bd42b08416c905872ae9
Eric Carty-Fickes <ericcf@northwestern.edu>
2015-11-25 13:18:32 -0600

    add release version to Raven config

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,11 +1,13 @@
 require "raven"
+require "conemo_dashboard"
 
 Raven.configure do |config|
   config.environments = %w( staging production )
-  config.excluded_exceptions = [
-    "URI::InvalidURIError",
-    "ActionController::UnknownHttpMethod"
-  ]
+  config.excluded_exceptions = %w(
+    URI::InvalidURIError
+    ActionController::UnknownHttpMethod
+  )
   dsn = Rails.application.config.try(:sentry_dsn)
   config.dsn = dsn if dsn
+  config.release = ConemoDashboard::VERSION
 end
