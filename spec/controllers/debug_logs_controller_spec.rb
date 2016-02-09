e85eb1c711d190ff7a50c272d843c37d894d08e8
Eric Carty-Fickes <ericcf@northwestern.edu>
2016-02-08 18:55:43 -0600

    stop dumping pr debug log files
    [#113295489]

--- spec/controllers/debug_logs_controller_spec.rb
+++ spec/controllers/debug_logs_controller_spec.rb
@@ -2,30 +2,15 @@ require "spec_helper"
 
 RSpec.describe DebugLogsController, type: :controller do
   describe "POST create" do
-    fixtures :all
+    it "responds with 200" do
+      post :create,
+           json: {
+             timestamp: Time.zone.now.to_i,
+             user_id: "foo",
+             event_type: "bar"
+           }.to_json
 
-    it "generates a log file" do
-      log_path = Rails.root.join("log/debug_logs")
-
-      expect do
-        post :create,
-             json: {
-               timestamp: Time.zone.now.to_i,
-               user_id: "foo",
-               event_type: "bar"
-             }.to_json
-      end.to change { Dir[Rails.root.join("log/debug_logs")].count }.by(1)
-
-      Dir["#{ log_path }/USER*"].each { |f| File.delete(f) }
-      Dir.delete(log_path)
-    end
-
-    context "when the parameters are malformed" do
-      it "returns a server error" do
-        post :create, json: "asdf"
-
-        expect(response.status).to eq 500
-      end
+      expect(response.status).to eq 200
     end
   end
 end
