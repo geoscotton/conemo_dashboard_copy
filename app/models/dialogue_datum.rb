1e3820a9aee5dc2058506efcfcb036c74e7be370
Eric Carty-Fickes <ericcf@northwestern.edu>
2016-02-18 15:45:02 -0600

    remove PRW related models and code

--- app/models/dialogue_datum.rb
+++
@@ -1,8 +0,0 @@
-# backed by PRW; imports into content access events
-class DialogueDatum < ActiveRecord::Base
-  establish_connection :prw
-
-  def content_access_event_exists?
-    ContentAccessEvent.exists?(dialogue_datum_guid: self.GUID)
-  end
-end
--- app/models/lesson_datum.rb
+++
@@ -1,9 +0,0 @@
-# backed by PRW; imports into content access events
-# Each of these represents the completion of a lesson.
-class LessonDatum < ActiveRecord::Base
-  establish_connection :prw
-
-  def content_access_event_exists?
-    ContentAccessEvent.exists?(lesson_datum_guid: self.GUID)
-  end
-end
