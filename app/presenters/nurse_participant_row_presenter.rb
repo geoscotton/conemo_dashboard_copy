0e4cacb46f5b8b9ff6483684545293b71e2ac52f
Eric Carty-Fickes <ericcf@northwestern.edu>
2016-02-25 09:47:15 -0600

    color code participant rows based on tasks
    [#112388677]

---
+++ app/presenters/nurse_participant_row_presenter.rb
@@ -0,0 +1,24 @@
+# Determine the appropriate CSS class to use for a Participant row on the Nurse
+# dashboard.
+class NurseParticipantRowPresenter
+  CSS_CLASSES = {
+    no_tasks: "success",
+    overdue_tasks: "danger",
+    current_tasks: "warning"
+  }.freeze
+
+  def initialize(participant, tasks)
+    @participant = participant
+    @tasks = tasks || []
+  end
+
+  def css_class
+    if @tasks.count == 0
+      CSS_CLASSES[:no_tasks]
+    elsif @tasks.any?(&:overdue?)
+      CSS_CLASSES[:overdue_tasks]
+    else
+      CSS_CLASSES[:current_tasks]
+    end
+  end
+end
--- spec/views/nurse_dashboards/show.html.erb_spec.rb
+++ spec/views/nurse_dashboards/show.html.erb_spec.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require "spec_helper"
+require_relative "../../../app/models/nurse_task"
+require_relative "../../../app/models/participant"
 
 RSpec.describe "nurse_dashboards/show", type: :view do
   def stub_participant(id)
@@ -14,6 +16,7 @@ RSpec.describe "nurse_dashboards/show", type: :view do
   let(:template) { "nurse_dashboards/show" }
   let(:participant1) { stub_participant 1 }
   let(:participant2) { stub_participant 2 }
+  let(:participant3) { stub_participant 3 }
 
   it "renders correctly when there are no participants" do
     assign(:participants, [])
@@ -24,9 +27,20 @@ RSpec.describe "nurse_dashboards/show", type: :view do
   end
 
   context "when there are participants" do
+    let(:current_task) do
+      instance_double(NurseTask,
+                      to_s: "Walk without rhythm",
+                      overdue?: false)
+    end
+    let(:overdue_task) do
+      instance_double(NurseTask,
+                      to_s: "Scat",
+                      overdue?: true)
+    end
+
     def assign_and_render
-      assign(:participants, [participant1, participant2])
-      assign(:tasks, 2 => ["Walk without rhythm", "Scat"])
+      assign(:participants, [participant1, participant2, participant3])
+      assign(:tasks, 2 => [current_task, overdue_task], 3 => [current_task])
 
       render template: template
     end
@@ -37,9 +51,28 @@ RSpec.describe "nurse_dashboards/show", type: :view do
       table_exists_with_the_following_rows(
         [
           ["ID 1", ""],
-          ["ID 2", "Walk without rhythm, Scat"]
+          ["ID 2", "Walk without rhythm, Scat"],
+          ["ID 3", "Walk without rhythm"]
         ]
       )
+    end
+
+    it "renders success rows for no tasks" do
+      assign_and_render
+
+      expect(rendered).to have_selector(".success", text: "ID 1")
+    end
+
+    it "renders warning rows for current tasks" do
+      assign_and_render
+
+      expect(rendered).to have_selector(".danger", text: "ID 2")
+    end
+
+    it "renders danger rows for overdue tasks" do
+      assign_and_render
+
+      expect(rendered).to have_selector(".warning", text: "ID 3")
     end
   end
 end
