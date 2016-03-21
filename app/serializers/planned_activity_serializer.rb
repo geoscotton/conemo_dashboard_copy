607b7f8ac0c044831e90899b1925bfa0fd0f902f
Eric Carty-Fickes <ericcf@northwestern.edu>
2016-03-21 08:41:42 -0500

    serialize all Planned Activity attributes
    [#115186005]

--- app/serializers/planned_activity_serializer.rb
+++ app/serializers/planned_activity_serializer.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 # PlannedActivity serializer.
 class PlannedActivitySerializer < ActiveModel::Serializer
-  attributes :name, :is_complete, :planned_at, :lesson_guid
+  attributes :name, :is_complete, :level_of_happiness, :how_worthwhile,
+             :planned_at, :lesson_guid, :follow_up_at
   attribute :uuid, key: :id
 end
