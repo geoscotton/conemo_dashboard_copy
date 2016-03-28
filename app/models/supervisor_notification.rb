d871aee6e936f7b06cd0c6ae122ecf05ed0ddb11
Eric Carty-Fickes <ericcf@northwestern.edu>
2016-03-28 16:07:31 -0500

    allow Supervisors to clear Notifications
    [#114741581]

--- app/models/supervisor_notification.rb
+++ app/models/supervisor_notification.rb
@@ -6,14 +6,24 @@ class SupervisorNotification < ActiveRecord::Base
 
   belongs_to :nurse_task
 
-  validates :nurse_supervisor, :nurse_task, :status, presence: true
+  validates :nurse_task, :status, presence: true
   validates :status, inclusion: { in: STATUSES.values }
+
+  scope :active, -> { where(status: STATUSES.active) }
 
   def self.latest_for(task_id)
     where(nurse_task_id: task_id).order(:created_at).last
   end
 
+  def self.active_for_nurse(nurse)
+    active.where(nurse_task: nurse.active_tasks)
+  end
+
   def nurse_supervisor
     nurse_task.nurse.nurse_supervisor
+  end
+
+  def resolve
+    update status: STATUSES.resolved
   end
 end
