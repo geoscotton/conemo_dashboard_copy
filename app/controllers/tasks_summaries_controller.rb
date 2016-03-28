3d30aa3813122ca30426555dbfa27972267ae4d0
Eric Carty-Fickes <ericcf@northwestern.edu>
2016-03-28 12:20:26 -0500

    show overdue and current tasks for nurses
    [#109669310] [#109669354]

---
+++ app/controllers/tasks_summaries_controller.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+# Manage Nurse Tasks display for Supervisors.
+class TasksSummariesController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
+  def show
+    @nurse_dashboard = NurseDashboardPresenter.new(find_nurse)
+    authorize! :read, @nurse_dashboard.participants
+  end
+
+  private
+
+  def find_nurse
+    @nurse ||= current_user.nurses.find(params[:nurse_id])
+  end
+
+  def record_not_found
+    redirect_to nurse_supervisor_dashboard_url,
+                alert: Nurse.model_name.human + " " + t(".not_found")
+  end
+end
