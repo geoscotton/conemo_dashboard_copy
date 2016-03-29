2e15316942cb3af1652aa77ac271f444ac4bd550
Eric Carty-Fickes <ericcf@northwestern.edu>
2016-03-29 16:33:45 -0500

    redirect non-Nurses to root from dashboard
    [#116522531]

--- app/controllers/nurse_dashboards_controller.rb
+++ app/controllers/nurse_dashboards_controller.rb
@@ -2,6 +2,8 @@
 # Manage Nurse display.
 class NurseDashboardsController < ApplicationController
   def show
+    return redirect_to(root_url) unless current_user.nurse?
+
     @nurse_dashboard = NurseDashboardPresenter.new(current_user)
     authorize! :read, @nurse_dashboard.participants
   end
