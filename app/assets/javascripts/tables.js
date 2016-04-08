21dd313755bd0b8043dc5a3859b20ecfe30d68c6
Eric Carty-Fickes <ericcf@northwestern.edu>
2016-04-07 21:18:26 -0500

    fix datatables application
    * apply only to specific tables
    * use turbolinks documentation
    
    [#117076811]

--- app/assets/javascripts/tables.js
+++ app/assets/javascripts/tables.js
@@ -1,6 +1,23 @@
-// see http://stackoverflow.com/questions/17147821/how-to-make-a-whole-row-in-a-table-clickable-as-a-link
-$(document).on("ready page:change", function() {
-  $(".clickable-row").click(function() {
-    Turbolinks.visit($(this).data("href"));
+(function() {
+  "use strict";
+
+  function addFilterAndPagination() {
+    if ($.fn.dataTable.isDataTable("table.filterable")) {
+      return;
+    }
+
+    $("table.filterable").DataTable({
+      "ordering": false,
+      "language": { "url": "<%= root_url %>/datatable.json" }
+    });
+  }
+
+  $(document).on("ready page:load", function() {
+    // see http://stackoverflow.com/questions/17147821/how-to-make-a-whole-row-in-a-table-clickable-as-a-link
+    $(".clickable-row").click(function() {
+      Turbolinks.visit($(this).data("href"));
+    });
+
+    addFilterAndPagination();
   });
-});
+})();
