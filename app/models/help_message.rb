4cb34fa151e3ecc96ba6eb36c18475d2d6fb2221
Eric Carty-Fickes <ericcf@northwestern.edu>
2016-08-03 13:43:46 -0500

    remove unused validation (#289)
    [#127646639]

--- app/models/help_message.rb
+++ app/models/help_message.rb
@@ -3,7 +3,6 @@
 class HelpMessage < ActiveRecord::Base
   belongs_to :participant
 
-  validates :read, inclusion: { in: [true, false] }
   validates :message, :sent_at, :participant, :uuid, presence: true
 
   before_validation :set_uuid
